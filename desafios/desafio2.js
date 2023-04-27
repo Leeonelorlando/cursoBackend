afdf512dacbc85f8e0a310bdfbcba5603d35b17a
Leonel Orlando <leonelorlando98@hotmail.com>
2023-04-27 17:45:25 -0300

    cambios

---
+++ desafios/desafio2.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+class UserManager {
+    constructor(path) {
+        this.users = []
+        this.path = path
+        this.init(path)
+    }
+    init(path) {
+        let file = fs.existsSync(path)
+        if (!file){
+        fs.promises.writeFile(path, '[]')
+        .then(res=>(console.log('file created')))
+        .catch(err=> console.log(err))
+    } else {
+        this.users = fs.promises.readFile(path)
+            .then(res=> this.users = JSON.parse(res))
+            .catch(err=> console.log(err))
+    }
+}
+
+    addUser({name, lastName, age, carts}) {
+        let data = {name, lastName, age, carts}
+        data = JSON.stringify(data, null, 2)
+        fs.promises.writeFile(this.path, data)
+            .then(res=>console.log('user created'))
+            .catch(err=>console.log(err))
+    }
+
+}
+
+let manager = new UserManager('./data/users.json')
+manager.addUser({name: 'Leonel', lastName: 'Orlando', age: '24', carts: []})
