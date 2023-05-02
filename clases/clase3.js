447580c211b48d201e2c8ce1d35cb9c9ba780da3
Leonel Orlando <leonelorlando98@hotmail.com>
2023-05-01 23:54:01 -0300

    cambios

--- clases/clase3.js
+++ clases/clase3.js
@@ -2,112 +2,97 @@ const fs = require('fs')
 
 class UserManager {
     constructor(path) {
-        this.users = []     //para guardar en la memoria todos los usuarios
-        this.path = path    //para guardar en la memoria la ruta del archivo
-        this.init(path)     //para iniciar la instancia y crear el archivo en caso de no existir o cargar la memoria en caso de existir usuarios
+        this.users = []
+        this.path = path
+        this.init(path)
     }
     init(path) {
-        //verifico si existe el archivo
         let file = fs.existsSync(path)
-        //console.log(file)
         if (!file) {
-            //si no existe lo creo
-            fs.writeFileSync(path,'[]')
-            console.log('file created at path: '+this.path)
-            return 'file created at path: '+this.path
+            fs.writeFileSync(path, '[]')
+            console.log('file created at path: ' + this.path)
+            return 'file created at path: ' + this.path
         } else {
             //si existe cargo los usuarios en la memoria del programa
-            this.users = JSON.parse(fs.readFileSync(path,'UTF-8'))
+            this.users = JSON.parse(fs.readFileSync(path,'utf-8'))
             console.log('data recovered')
             return 'data recovered'
         }
     }
-    async add_user({ name,last_name,age,carts }) {
-        try {
-            //defino el objeto que necesito agregar al array
-            let data = { name,last_name,age,carts }
-            //si la memoria tiene usuarios
-            if (this.users.length>0) {
-                //busco el id del último elemento y le sumo 1
-                let next_id = this.users[this.users.length-1].id+1
-                //agrego la propiedad al objeto
-                data.id = next_id
-            } else {
-                //en caso que no tenga: asigno el primer id
-                data.id = 1
-            }
-            //agrego el objeto (usuario) a la memoria del programa
-            this.users.push(data)
-            //convierto a texto plano el array
-            let data_json = JSON.stringify(this.users,null,2)
-            //sobre-escribo el archivo
-            await fs.promises.writeFile(this.path,data_json)
-            console.log('id´s created user: '+data.id)
-            return 'id´s user: '+data.id
-        } catch(error) {
-            console.log(error)
-            return 'error: creating user'
+
+async addUser({name, lastName, age, carts}) {
+    try {
+        let data = {name, lastName, age, carts}
+        if (this.users.length>0) {
+            let nextId = this.users[this.users.length-1].id+1
+            data.id = nextId
+        } else {
+            data.id = 1
         }
+        this.users.push(data)
+        let dataJson = JSON.stringify(this.users, null, 2)
+        await fs.promises.writeFile(this.path,dataJson)
+        console.log('id´s created user: '+ data.id)
+        return 'id´s user: '+ data.id
+    } catch(error) {
+        console.log(error)
+        return 'error: creating user'
     }
-    read_users() {
-        //console.log(this.users)
+}
+
+    readUsers() {
         return this.users
     }
-    read_user(id) {
+
+    readUser(id) {
         let one = this.users.find(each=>each.id===id)
-        //console.log(one)
         return one
     }
-    async update_user(id,data) {
-        //data es el objeto con las propiedades que necesito modificar del usuario
+
+    async updateUser(id, data) {
         try {
-            //busco el usuario
-            let one = this.read_user(id)
-            //itero para modificar la propiedad correspondiente
+            let one = this.readUser(id)
+
             for (let prop in data) {
-                //console.log(prop)
-                one[prop] = data[prop]
+                one[prop] = data [prop]
             }
-            //convierto a texto plano el array
-            let data_json = JSON.stringify(this.users,null,2)
-            //sobre-escribo el archivo
-            await fs.promises.writeFile(this.path,data_json)
-            console.log('updated user: '+id)
-            return 'updated user: '+id
+
+            let dataJson = JSON.stringify(this.users, null, 2)
+
+            await fs.promises.writeFile(this.path, dataJson)
+            console.log('updated user: ' + id)
+            return 'updated user: ' + id
         } catch(error) {
             console.log(error)
             return 'error: updating user'
         }
     }
-    async destroy_user(id) {
+
+    async destroyUser(id) {
         try {
-            //saco el usuario
             this.users = this.users.filter(each=>each.id!==id)
-            //console.log(this.users)
-            //convierto a texto plano el array
-            let data_json = JSON.stringify(this.users,null,2)
-            //sobre-escribo el archivo
-            await fs.promises.writeFile(this.path,data_json)
-            console.log('delete user: '+id)
-            return 'delete user: '+id
+            let dataJson = JSON.stringify(this.users, null, 2)
+            await fs.promises.writeFile(this.path, dataJson)
+            console.log('delete user: ' + id)
+            return 'delete user: ' + id
         } catch(error) {
             console.log(error)
             return 'error: deleting user'
         }
     }
+
 }
 
 async function manager() {
     let manager = new UserManager('./data/users.json')
-    await manager.add_user({ name:'igna',last_name:'borraz',age:32,carts:[] })
-    await manager.add_user({ name:'nico',last_name:'lopez',age:37,carts:[] })
-    await manager.add_user({ name:'igna',last_name:'chapero',age:100,carts:[] })
-    await manager.add_user({ name:'mario',last_name:'castro',age:35,carts:[] })
-    await manager.add_user({ name:'luis',last_name:'aguilar',age:35,carts:[] })
-    await manager.update_user(1,{ name:'ignacio' })
-    await manager.update_user(2,{ name:'nicolas', carts: ['celular'] })
-    await manager.update_user(3,{ age:30 })
-    await manager.destroy_user(1)
-    await manager.destroy_user(4)
+    await manager.addUser({name: 'Leonel', lastName: 'Orlando', age: '24', carts: []})
+    await manager.addUser({name: 'Prueba', lastName: 'Usuario2', age: '20', carts: []})
+    await manager.addUser({name: 'Carlos', lastName: 'Alcaraz', age: '25', carts: []})
+    await manager.addUser({name: 'Esteban', lastName: 'Rinaudo', age: '33', carts: []})
+    await manager.updateUser(1, {name: 'Leonel'})
+    await manager.updateUser(2, {name: 'Prueba', carts: ['celular']})
+    await manager.updateUser(3, {age: 30})
+    await manager.destroyUser(1)
+    await manager.destroyUser(4)
 }
 manager()
--- desafios/desafio2.js
+++ desafios/desafio2.js
@@ -1,97 +1,119 @@
 const fs = require('fs')
 
-class UserManager {
+class ProductManager {
     constructor(path) {
-        this.users = []
+        this.products = []
         this.path = path
         this.init(path)
     }
+
     init(path) {
         let file = fs.existsSync(path)
-        if (!file){
-        fs.promises.writeFile(path, '[]')
-        .then(res=>(console.log('file created')))
-        .catch(err=> console.log(err))
-    } else {
-        fs.promises.readFile(path, 'utf-8')
-            .then(res=> this.users = JSON.parse(res))
-            .catch(err=> console.log(err))
-    }
-}
-
-async addUser({name, lastName, age, carts}) {
-    try {
-        let data = {name, lastName, age, carts}
-        if (this.users.length>0) {
-            let nextId = this.users[this.users.length-1].id+1
-            data.id = nextId
+        if (!file) {
+            fs.writeFileSync(path,'[]')
+            console.log('file created at path: '+this.path)
+            return 'file created at path: '+this.path
         } else {
-            data.id = 1
+            this.products = JSON.parse(fs.readFileSync(path,'UTF-8'))
+            console.log('data recovered')
+            return 'data recovered'
         }
-        this.users.push(data)
-        let dataJson = JSON.stringify(this.users, null, 2)
-        await fs.promises.writeFile(this.path,dataJson)
-        console.log('id´s created user: '+ data.id)
-        return 'id´s user: '+ data.id
-    } catch(error) {
-        console.log(error)
-        return 'error: creating user'
-    }
-}
-
-    readUsers() {
-        return this.users
     }
 
-    readUser(id) {
-        let one = this.users.find(each=>each.id===id)
-        return one
-    }
-
-    async updateUser(id, data) {
+    async addProduct({title, description, price, thumbnail, code, stock}) {
         try {
-            let one = this.readUser(id)
-
-            for (let prop in data) {
-                one[prop] = data [prop]
+            let data = {title, description, price, thumbnail, code, stock}
+            if (this.products.length > 0) {
+                let next_id = this.products[this.products.length-1].id + 1
+                data.id = next_id
+            } else {
+                data.id = 1
             }
-
-            let dataJson = JSON.stringify(this.users, null, 2)
-
-            await fs.promises.writeFile(this.path, dataJson)
-            console.log('updated user: ' + id)
-            return 'updated user: ' + id
+            this.products.push(data)
+            let data_json = JSON.stringify(this.products,null,2)
+            await fs.promises.writeFile(this.path,data_json)
+            console.log('created product: '+ data.id)
+            return 'product: '+ data.id
         } catch(error) {
             console.log(error)
-            return 'error: updating user'
+            return 'addProduct: error'
         }
     }
 
-    async destroyUser(id) {
-        try {
-            this.users = this.users.filter(each=>each.id!==id)
-            let dataJson = JSON.stringify(this.users, null, 2)
-            await fs.promises.writeFile(this.path, dataJson)
-            console.log('delete user: ' + id)
-            return 'delete user: ' + id
+    getProducts() {
+        try{
+            return this.products
         } catch(error) {
             console.log(error)
-            return 'error: deleting user'
+            return 'getProduct: error'
+        }
+    }
+
+    getProductById(id) {
+        let one = this.products.find(each=>each.id===id)
+        if(!one) {
+            console.log('getProductById: error')
+            return null
+        } else{
+            console.log('finded product: ' + id)
+            return one
+        }
+    }
+
+    async updateProduct(id, data) {
+        try {
+            let one = this.getProductById(id)
+            if(!one) {
+                console.log('Not found')
+                return 'Not found'
+            }
+            if(Object.keys(data).length===0) {
+                console.log('error: insert some product')
+                return 'error: insert some product'
+            }
+            for (let prop in data) {
+                one[prop] = data[prop]
+            }
+            let data_json = JSON.stringify(this.products,null,2)
+            await fs.promises.writeFile(this.path,data_json)
+            console.log('updatedProduct: '+ id)
+            return 'updatedProduct: '+ id
+        } catch(error) {
+            console.log(error)
+            return 'updateProduct: error'
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+            let one = this.getProductById(id)  
+            if(!one) {
+                console.log('Not found')
+                return 'Not found'
+            }
+            this.products = this.products.filter(each=>each.id!==id)
+            let data_json = JSON.stringify(this.products,null,2)
+            await fs.promises.writeFile(this.path,data_json)
+            console.log('deleteProduct: '+id)
+            return 'deleteProduct: '+id
+        } catch(error) {
+            console.log(error)
+            return 'deleteProduct: error'
         }
     }
 
 }
 
 async function manager() {
-    let manager = new UserManager('./data/users.json')
-    await manager.addUser({name: 'Leonel', lastName: 'Orlando', age: '24', carts: []})
-    await manager.addUser({name: 'Prueba', lastName: 'Usuario2', age: '20', carts: []})
-    await manager.addUser({name: 'Carlos', lastName: 'Alcaraz', age: '25', carts: []})
-    await manager.addUser({name: 'Esteban', lastName: 'Rinaudo', age: '33', carts: []})
-    await manager.updateUser(1, {name: 'Leonel'})
-    await manager.updateUser(2, {name: 'Prueba', carts: ['celular']})
-    await manager.updateUser(3, {age: 30})
-    await manager.destroyUser(1)
-    await manager.destroyUser(4)
+    let manager = new ProductManager('./data/data.json')
+    await manager.addProduct({ title:"cheeseburg", description:"solo queso", price:10, thumbnail:"foto cheese", code:"che", stock:4 })
+    await manager.addProduct({ title:"doble cheese", description:"mucho queso", price:5, thumbnail:"foto doble cheese", code:"chs", stock:10 })
+    await manager.addProduct({ title:"onion burger", description:"con cebolla", price:20, thumbnail:"foto onion", code:"oni", stock:8 })
+    await manager.addProduct({ title:"american", description:"con tomate y lechuga", price:12, thumbnail:"foto american", code:"ame", stock:6 })
+    await manager.updateProduct(0,{ title:"cheeseburger" })
+    await manager.updateProduct(1,{title:"doble cheeseburger", stock:11})
+    await manager.updateProduct(2,{})  
+    await manager.deleteProduct(1)
+    await manager.deleteProduct(130)
 }
 manager()
