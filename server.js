96056576a7164a93cfb150863ef5e2b796f2f4dc
Leonel Orlando <leonelorlando98@hotmail.com>
2023-05-02 23:14:21 -0300

    cart

--- server.js
+++ server.js
@@ -1,58 +1,106 @@
-import express from 'express'
-import manager from './desafios/app.js'
+import express, { response } from 'express'
+import manager from "./desafios/app.js"
+import cart from "./desafios/cart.js"
 
-let server = express()
+const server = express()
 
-let PORT = 8080
-let ready = ()=>console.log('server ready on port: '+PORT)
+const PORT = 8080
+const ready = ()=> console.log('server ready on port ' + PORT)
 
 server.listen(PORT,ready)
+server.use(express.json())
 server.use(express.urlencoded({extended:true}))
 
-let index_route = '/'
-let index_function = (req,res)=> {
-    let quantity = manager.getProducts().length
-    console.log(quantity)
-    return res.send(`there are ${quantity} products`)
+let index_route ="/"
+let index_function =(req,res)=>{
+    let products= manager.getProducts().length
+    console.log(products)
+    return res.send(`Hay ${products} productos`)
 }
 server.get(index_route,index_function)
 
-let one_route = '/products/:id'
-let one_function = (request,response)=> {
-    let parametros = request.params
-    let id = Number(parametros.id)
-    let one = manager.getProductById(id)
-    console.log(one)
-    if (one) {
-        return response.send({
-            success: true,
-            product: one
+let products_route =  "/api/products"
+let products_function=(req,res)=>{
+    let limit=req.query.limit
+    console.log(limit)
+    if(limit){
+        let products = manager.getProducts().slice(0,limit)
+    if(products.length>0){
+        return res.send({
+            succes: true,
+            products: products
         })
-    } else {
-        return response.send({
-            success: false,
-            product: 'not found'
-        })
+    }else{
+    return res.send({
+        succes: false,
+        products: "not found"
+    })}
+    }else{
+        let products = manager.getProducts()
+        if(products.length>0){
+            return res.send({
+                succes: true,
+                products: products
+            })
+        }else{
+        return res.send({
+            succes: false,
+            products: "not found"
+        })}  
     }
     
 }
-server.get(one_route,one_function)
+server.get(products_route,products_function)
 
-let query_route = '/products'
-let query_function = (req,res)=> {
-    console.log(req.query)
-    let quantity = req.query.quantity ?? 7
-    let products = manager.getProducts().slice(0,quantity) //array de usuarios que tengo que REBANAR para que se pagine según la query que envía el cliente
-    if (products.length>0) {
+let id_route= "/api/products/:id"
+let id_function=(req,res)=>{
+    let parametros = req.params
+    let id = Number(parametros.id)
+    let one=manager.getProductById(id)
+    console.log(one)
+    if(one){
         return res.send({
-            success: true,
-            products
+            succes: true,
+            product : one
         })
-    } else {
+    }else{
         return res.send({
-            success: false,
-            products: 'not found'
+            succes: false,
+            product : "not found"
         })
-    }    
+    }
 }
-server.get(query_route,query_function)
+server.get(id_route,id_function)
+
+let cart_route="/api/carts"
+let cart_function=(req,res)=>{
+    let carrito= cart.getCarts()
+    console.log(carrito)
+    return res.send({
+        succes:true,
+        response:carrito
+
+    })
+
+}
+server.get(cart_route,cart_function)
+
+let cid_route= "/api/carts/:cid"
+let cid_function=(req,res)=>{
+    let parametros = req.params
+    let id = Number(parametros.cid)
+    let one=cart.getCartsById(id)
+    console.log(one)
+    if(one){
+        return res.send({
+            succes: true,
+            user : one
+        })
+    }else{
+        return res.send({
+            succes: false,
+            user : "not found"
+        })
+    }
+}
+server.get(cid_route,cid_function)
