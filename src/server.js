0ad97dc5eded1125b1372c21da59f4d1eb696785
Leonel Orlando <leonelorlando98@hotmail.com>
2023-05-17 21:51:02 -0300

    agregamos app

--- src/server.js
+++ src/server.js
@@ -1,24 +1,16 @@
-import express from 'express'
-import router from './routes/index.js'
-import error_handler from './middlewares/error_handler.js'
-import not_found_handler from './middlewares/not_found.js'
-import { __dirname } from './utils.js'
-import { engine } from 'express-handlebars'
-
-const server = express()
+import server from "./app.js"
+import { Server } from "socket.io"
 
 const PORT = 8080
 const ready = ()=> console.log('server ready on port '+PORT)
 
-server.engine('handlebars',engine())
-server.set('views',__dirname+'/views')
-server.set('view engine','handlebars')
+let http_server = server.listen(PORT,ready)
+let socket_server = new Server(http_server)
 
-server.use(express.static('public'))
-server.use(express.json())
-server.use(express.urlencoded({extended:true}))
-server.use('/',router)
-server.use(error_handler)
-server.use(not_found_handler)
-
-server.listen(PORT,ready)
+socket_server.on(
+    'client_connected',
+    socket => {
+        console.log(socket)
+        console.log('client connected')
+    }
+)
